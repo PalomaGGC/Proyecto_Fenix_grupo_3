159a5cfaa05d9eeddc7cc52d0825f476b5d2341f
maikol <mgarrido2503@gmail.com>
2023-07-07 09:18:41 +0200

    se agrego la carpeta de nivelesModel y packModels

---
+++ src/models/pack.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Boolean, Numeric, Table, Column, String
+from sqlalchemy.sql.sqltypes import Integer
+from config.db import meta, engine
+
+
+#creo el modelo de la tabla
+tabla_pack = Table("pack", meta, 
+    Column("id_pack", Integer, primary_key=True,  autoincrement=True),
+    Column("nombre_pack", String(100)),
+    Column("precio_pack", float),
+)
+
+#creo la tabla en la base de datos
+meta.create_all(engine)
