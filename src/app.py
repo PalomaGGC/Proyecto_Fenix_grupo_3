3dc067dd7877776c25d4d8558dd84762f63fb67a
verobermejo <vero_bermejo@hotmail.com>
2023-07-11 13:52:08 +0200

    se agrego primer y segundo descuento a la base de datos

--- src/app.py
+++ src/app.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
 from routes.alumnos_routes import alumnos
-
+from routes.pack_routes import packs
 app = FastAPI()
 
 app.include_router(alumnos)
-
+app.include_router(packs)
 
 # https://github.com/AI-School-F5-P2/Proyecto_Fenix_equipo_3.gitgit remote -v
