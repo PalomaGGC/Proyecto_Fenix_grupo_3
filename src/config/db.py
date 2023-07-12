3e4aa220167d8099bdc05137383d20e9c586f8b2
maikol <mgarrido2503@gmail.com>
2023-07-12 12:03:10 +0200

    .

--- src/config/db.py
+++ src/config/db.py
@@ -1,9 +1,17 @@
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.exc import SQLAlchemyError
+from decouple import config
 
 try:
+    user= config("USER")
+    host= config("HOST")
+    db= config("DB")
+    url = f"mysql+pymysql://{user}:@{host}:3306/{db}"
+    
     # Aquí hago la conexión a la base de datos... la base de datos se llama "database_fenix"
-    engine = create_engine("mysql+pymysql://root:@localhost:3306/database_fenix")
+    # engine = create_engine("mysql+pymysql://root:@localhost:3306/database_fenix")
+    engine = create_engine(url)
+    print(url)
     # Guardo la conexión en una variable para luego utilizarla en otros archivos
     conexion = engine.connect()
     # MetaData actúa como un contenedor para mantener información sobre las tablas, columnas,
