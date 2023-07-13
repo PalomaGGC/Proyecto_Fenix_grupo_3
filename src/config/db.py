a55862aaeac2b238fc77418c8fda3bf9d4c5ff85
Paloma Garcia <paloma.ggc@gmail.com>
2023-07-13 10:15:36 +0200

    Vuelto a cambiar print(user) a print(url)

--- src/config/db.py
+++ src/config/db.py
@@ -7,10 +7,10 @@ try:
     host= config("HOST")
     db= config("DB")
     url = f"mysql+pymysql://{user}:@{host}:3306/{db}"
-    
+
     # Aquí hago la conexión a la base de datos... la base de datos se llama "database_fenix"
     engine = create_engine(url)
-    print(user)
+    print(url)
     # Guardo la conexión en una variable para luego utilizarla en otros archivos
     conexion = engine.connect()
     # MetaData actúa como un contenedor para mantener información sobre las tablas, columnas,
