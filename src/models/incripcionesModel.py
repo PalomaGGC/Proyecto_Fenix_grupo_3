d2001afc88d3e53f41aeffd89b8e24f492564e76
maikol <mgarrido2503@gmail.com>
2023-07-17 14:30:41 +0200

    relaciono tabla profesors_clase con inscripciones

--- src/models/incripcionesModel.py
+++ src/models/incripcionesModel.py
@@ -1,16 +1,16 @@
-from sqlalchemy import ForeignKey, Column,String
-from sqlalchemy.sql.sqltypes import Integer
+from sqlalchemy import DateTime, ForeignKey, Column,String, Integer,func
 from config.db import Base
 
 #MODELO DE LA TABLA
 class Inscripciones_model(Base):
     __tablename__ = "inscripciones"
     id_inscripciones = Column(Integer, primary_key=True,  autoincrement=True)
-    alumnos_id = Column(Integer, ForeignKey("alumnos.id_alumno"))
-    profesor_clase_id = Column(Integer)
+    alumno_id = Column(Integer, ForeignKey("alumnos.id_alumno"))
+    profesor_clase_id = Column(Integer, ForeignKey("profesores.id_clase_profesor"))
     precio_clase = Column(Integer)
     descuento_inscripcion = Column(Integer)
     precio_con_descuento = Column(Integer)
     estado_inscripcion = Column(String(10))
-    fecha_inscripcion = Column(String(20))
+    pack_id = Column(Integer)
+    fecha_inscripcion = Column(DateTime, default=func.now())
 
