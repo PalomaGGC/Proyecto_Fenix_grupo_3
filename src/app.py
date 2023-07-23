9b1f2ef7c7d690b44d0b31fe540c4b8426a227c7
maikol <mgarrido2503@gmail.com>
2023-07-23 19:28:39 +0200

    agrego la funcion inscripcion_autamica

--- src/app.py
+++ src/app.py
@@ -1,4 +1,7 @@
+import schedule
+import uvicorn
 from fastapi import FastAPI
+import threading
 from fastapi.testclient import TestClient
 from routers.alumnos_router import alumnos
 from routers.clases_router import clases
@@ -10,7 +13,8 @@ from routers.clases_router import clases
 from routers.niveles_router import niveles
 from decouple import config
 from config.db import Base, engine
-import uvicorn
+from services.incripcion_automatica_services import crear_nueva_inscripcion
+import time
 
 app = FastAPI()
 port = config("PORT") 
@@ -24,7 +28,17 @@ app.include_router(inscripciones)
 app.include_router(profesor_clases)
 
 Base.metadata.create_all(bind=engine)
-    
 
-if __name__ == '__main__':
-    uvicorn.run("app:app", port=int(port), host='localhost', reload=True)
+#GENERAR NUEVAS INSCRIPCIONES AUTOMATICAMENTE
+def programar_creacion_nueva_inscripcion():
+    schedule.every().day.at("19:20:10").do(crear_nueva_inscripcion)
+    # Programo la ejecución de la función "crear_nueva_inscripcion" cada día a la medianoche
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+        
+# Utilizo threading para ejecutar la función en un hilo separado
+creacion_inscripciones_thread = threading.Thread(target=programar_creacion_nueva_inscripcion)
+creacion_inscripciones_thread.start()
+
+# uvicorn app:app --host localhost --port 5000 --reload
